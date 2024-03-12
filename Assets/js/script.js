f64066ce0bcf5734fd4ce871e9f06ed47f7d8a2b
Ralph Molu <rmolu11@gmail.com>
2024-03-12 15:37:27 -0400

    using 2 URLs: one to manage user city entry and another to get geolocation. Formatted date by converting from Unix format to human readable format

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,57 @@
 
 const APIKey = '48d2564f931d9d89947234ab9a175583';
-let city = "London";
+let city = 'Durham';
+let countryCode;
 
-var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${APIKey}`;
+var citySearchURL = `https:api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${APIKey}`;
 
-fetch(queryURL)
+
+
+// this function takes the user input, fetches the coordinates of the city entered and returns the weather using the coordinates
+// function getWeather(){}
+fetch(citySearchURL)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
     console.log(data);
+    
+    //variables to store longitude (lon) and lattitude (lat) of entered city
+    var lon = data.city.coord.lon;
+    var lat = data.city.coord.lat;
+    
+    // using the geolocation to pull weather information
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+    fetch(queryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        //display the date in simplified format from Unix Timestamp conversion
+        const date = new Date(data.list[0].dt * 1000); // convert unix seconds to milliseconds and store under Date object instance
+
+        //extracting date components
+        var month = date.getMonth() +1; // months in this method are indexed starting at 0 for Jan, 1 for Feb...
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        var simplifiedDate = `${month}/${day}/${year}`;
+
+        console.log(data.city.name +" (" + simplifiedDate + ")" );
+        console.log("Temp: " + data.list[0].main.temp + " °K");
+        console.log("Wind: " + data.list[0].wind.speed + " MPH");
+        console.log("Humidity: " + data.list[0].main.humidity + " %");
+        console.log("lat: " + lat)
+        console.log("lon: " + lon)
+
+    })
+
+
+
+
+    //create 
+   
+    
 });
+
